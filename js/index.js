f9bd7ff6f4bd6805934dc3d84cb3d9b2e727279b
minamifan <122370462+minamifan@users.noreply.github.com>
2024-01-13 19:08:34 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -36,3 +36,18 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwoRcCNPqWm4aiQFepprv
       .catch(error => console.error('Error!', error.message))
   })
   
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
